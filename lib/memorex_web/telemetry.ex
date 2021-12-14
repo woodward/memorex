2bc6864407d3fef194c9caf17d49d556a9c7ad3a
Greg Woodward <woodward@users.noreply.github.com>
2021-12-14 12:24:14 -0800

    Changes from 'mix format'

--- lib/memorex_web/telemetry.ex
+++ lib/memorex_web/telemetry.ex
@@ -49,8 +49,7 @@ defmodule MemorexWeb.Telemetry do
       ),
       summary("memorex.repo.query.idle_time",
         unit: {:native, :millisecond},
-        description:
-          "The time the connection spent waiting before being checked out for the query"
+        description: "The time the connection spent waiting before being checked out for the query"
       ),
 
       # VM Metrics
