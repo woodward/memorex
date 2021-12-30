5974b80972f16a1baeb84c85335c32d68eeba798
Greg Woodward <woodward@users.noreply.github.com>
2021-12-30 13:04:52 -0800

    Silence some credo warnings

--- lib/memorex/mailer.ex
+++ lib/memorex/mailer.ex
@@ -1,3 +1,4 @@
 defmodule Memorex.Mailer do
+  @moduledoc false
   use Swoosh.Mailer, otp_app: :memorex
 end
--- lib/memorex_web/telemetry.ex
+++ lib/memorex_web/telemetry.ex
@@ -1,4 +1,5 @@
 defmodule MemorexWeb.Telemetry do
+  @moduledoc false
   use Supervisor
   import Telemetry.Metrics
 
