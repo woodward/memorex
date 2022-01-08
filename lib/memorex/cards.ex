bcdf984899b5230a9270c92c410f15dcbeb86801
Greg Woodward <woodward@users.noreply.github.com>
2022-01-08 09:36:33 -0800

    The start of Cards.is_card_due?/2

--- lib/memorex/cards.ex
+++ lib/memorex/cards.ex
@@ -12,4 +12,14 @@ defmodule Memorex.Cards do
   def next_intervals(_card) do
     nil
   end
+
+  @spec is_card_due?(Card.t(), DateTime.t() | nil) :: boolean()
+  def is_card_due?(%Card{card_queue: :new} = _card, _now), do: true
+
+  def is_card_due?(card, now) do
+    case DateTime.compare(card.due, now) do
+      :lt -> true
+      _ -> false
+    end
+  end
 end
