a557c0c3de5e265b7f8ebc4770237de958a23cc2
Greg Woodward <woodward@users.noreply.github.com>
2022-05-29 07:46:12 -0700

    Re-ordered the config values

--- lib/memorex/config.ex
+++ lib/memorex/config.ex
@@ -7,10 +7,14 @@ defmodule Memorex.Config do
           learn_ahead_time_interval: Duration.t(),
           #
           learn_steps: [Duration.t()],
+          graduating_interval_good: Duration.t(),
+          graduating_interval_easy: Duration.t(),
+          #
           relearn_steps: [Duration.t()],
           #
           initial_ease: float(),
           #
+          max_reviews_per_day: non_neg_integer(),
           easy_multiplier: float(),
           hard_multiplier: float(),
           lapse_multiplier: float(),
@@ -19,24 +23,26 @@ defmodule Memorex.Config do
           max_review_interval: Duration.t(),
           min_review_interval: Duration.t(),
           #
-          graduating_interval_good: Duration.t(),
-          graduating_interval_easy: Duration.t(),
-          #
-          timezone: String.t(),
           #
           leech_threshold: non_neg_integer(),
           #
           min_time_to_answer: Duration.t(),
-          max_time_to_answer: Duration.t()
+          max_time_to_answer: Duration.t(),
+          #
+          timezone: String.t()
         }
 
   defstruct learn_ahead_time_interval: Duration.parse!("PT20M"),
             #
             learn_steps: [Duration.parse!("PT1M"), Duration.parse!("PT10M")],
+            graduating_interval_good: Duration.parse!("P1D"),
+            graduating_interval_easy: Duration.parse!("P4D"),
+            #
             relearn_steps: [Duration.parse!("PT10M")],
             #
             initial_ease: 2.5,
             #
+            max_reviews_per_day: 9999,
             easy_multiplier: 1.3,
             hard_multiplier: 1.2,
             lapse_multiplier: 0.0,
@@ -45,13 +51,10 @@ defmodule Memorex.Config do
             max_review_interval: Duration.parse!("P100Y"),
             min_review_interval: Duration.parse!("P1D"),
             #
-            graduating_interval_good: Duration.parse!("P1D"),
-            graduating_interval_easy: Duration.parse!("P4D"),
-            #
-            timezone: Timex.Timezone.Local.lookup(),
-            #
             leech_threshold: 8,
             #
             min_time_to_answer: Duration.parse!("PT1S"),
-            max_time_to_answer: Duration.parse!("PT1M")
+            max_time_to_answer: Duration.parse!("PT1M"),
+            #
+            timezone: Timex.Timezone.Local.lookup()
 end
