db9020171ccf0156659677703b5b1d637ebbe317
Greg Woodward <woodward@users.noreply.github.com>
2022-05-29 07:59:20 -0700

    Get rid of the functions on Cards; starting over...

--- lib/memorex/cards.ex
+++ lib/memorex/cards.ex
@@ -1,15 +1,3 @@
 defmodule Memorex.Cards do
   @moduledoc false
-
-  alias Memorex.Cards.Card
-
-  @spec answer_card(Card.t(), Card.answer_choice(), Timex.Duration.t()) :: nil
-  def answer_card(_card, _choice, _time_to_answer) do
-    nil
-  end
-
-  @spec next_intervals(Card.t()) :: nil
-  def next_intervals(_card) do
-    nil
-  end
 end
--- test/memorex/cards_test.exs
+++ test/memorex/cards_test.exs
@@ -1,19 +1,4 @@
 defmodule Memorex.CardsTest do
   @moduledoc false
   use Memorex.DataCase
-
-  alias Memorex.Cards
-  alias Memorex.Cards.Card
-
-  test "next_intervals/1" do
-    card = %Card{}
-    Cards.next_intervals(card)
-  end
-
-  test "answer_card/3" do
-    card = %Card{}
-    answer_choice = :again
-    time_to_answer = nil
-    Cards.answer_card(card, answer_choice, time_to_answer)
-  end
 end
