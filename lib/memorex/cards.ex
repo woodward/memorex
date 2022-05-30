de5dcbe1e1bc401b68d919c921faadff22adbb21
Greg Woodward <woodward@users.noreply.github.com>
2022-05-30 12:40:53 -0700

    Created Cards.update_new_cards_to_learn_cards

--- lib/memorex/cards.ex
+++ lib/memorex/cards.ex
@@ -2,7 +2,7 @@ defmodule Memorex.Cards do
   @moduledoc false
 
   alias Memorex.Cards.Card
-  alias Memorex.Repo
+  alias Memorex.{Config, Repo}
 
   @spec update_card!(Card.t(), map(), DateTime.t()) :: Card.t()
   def update_card!(card, changes, time) do
@@ -10,5 +10,23 @@ defmodule Memorex.Cards do
     |> Card.changeset(changes)
     |> Card.set_due_field_in_changeset(time)
     |> Repo.update!()
+  end
+
+  @spec update_new_cards_to_learn_cards(Ecto.Queryable.t(), Config.t(), DateTime.t(), Keyword.t()) :: :ok
+  def update_new_cards_to_learn_cards(queryable, config, time_now, opts \\ []) do
+    first_learn_step = config.learn_steps |> List.first()
+
+    updates = [
+      interval: first_learn_step,
+      remaining_steps: length(config.learn_steps),
+      card_type: :learn,
+      ease_factor: config.initial_ease,
+      lapses: 0,
+      reps: 0,
+      card_queue: :learn,
+      due: Timex.shift(time_now, duration: first_learn_step)
+    ]
+
+    Repo.update_all(queryable, [set: updates], opts)
   end
 end
--- test/memorex/cards_test.exs
+++ test/memorex/cards_test.exs
@@ -2,7 +2,7 @@ defmodule Memorex.CardsTest do
   @moduledoc false
   use Memorex.DataCase
 
-  alias Memorex.Cards
+  alias Memorex.{Cards, Config, Repo}
   alias Memorex.Cards.Card
   alias Timex.Duration
 
@@ -17,6 +17,31 @@ defmodule Memorex.CardsTest do
 
       assert updated_card.due == ~U[2022-02-11 12:00:00Z]
       assert updated_card.interval == Duration.parse!("P10D")
+    end
+  end
+
+  describe "update_new_cards_to_learn_cards/3" do
+    test "sets the values based on the config" do
+      config = %Config{
+        learn_steps: [Duration.parse!("PT2M"), Duration.parse!("PT15M")],
+        initial_ease: 2.25
+      }
+
+      card1 = %Card{card_type: :new, ease_factor: 2.15, lapses: 2, reps: 33, card_queue: :review} |> Repo.insert!()
+
+      time_now = ~U[2022-02-01 12:00:00Z]
+      Cards.update_new_cards_to_learn_cards(Card, config, time_now)
+
+      card1 = Repo.get!(Card, card1.id)
+
+      assert card1.interval == Duration.parse!("PT2M")
+      assert card1.remaining_steps == 2
+      assert card1.card_type == :learn
+      assert card1.card_queue == :learn
+      assert card1.ease_factor == 2.25
+      assert card1.lapses == 0
+      assert card1.reps == 0
+      assert card1.due == ~U[2022-02-01 12:02:00Z]
     end
   end
 end
