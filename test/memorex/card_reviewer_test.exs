07b45e627c2f3d0e388cf41b8e4742174ab431da
Greg Woodward <woodward@users.noreply.github.com>
2022-05-30 12:41:14 -0700

    Start of CardReviewer.answer_card

--- test/memorex/card_reviewer_test.exs
+++ test/memorex/card_reviewer_test.exs
@@ -2,7 +2,8 @@ defmodule Memorex.CardReviewerTest do
   @moduledoc false
   use Memorex.DataCase
 
-  alias Memorex.{CardReviewer, Config}
+  alias Memorex.{Cards, CardReviewer, Config}
+  alias Memorex.Cards.Card
   alias Timex.Duration
 
   describe "bracket_time_to_answer/1" do
@@ -49,6 +50,32 @@ defmodule Memorex.CardReviewerTest do
       start_time = ~U[2022-01-01 12:00:00Z]
       end_time = ~U[2022-01-01 12:02:30Z]
       assert CardReviewer.time_to_answer(start_time, end_time, config) == Duration.parse!("PT1M")
+    end
+  end
+
+  describe "answer_card" do
+    test "answers the card" do
+      config = %Config{
+        learn_steps: [Duration.parse!("PT2M"), Duration.parse!("PT15M")],
+        initial_ease: 2.4
+      }
+
+      card = Repo.insert!(%Card{card_type: :new})
+
+      time_now = ~U[2022-01-02 12:00:00Z]
+      Cards.update_new_cards_to_learn_cards(Card, config, time_now)
+      card = Repo.get!(Card, card.id)
+
+      card = CardReviewer.answer_card(card, :again, time_now, config)
+
+      assert card.card_type == :learn
+      assert card.card_queue == :learn
+      assert card.remaining_steps == 2
+      assert card.due == ~U[2022-01-02 12:02:00Z]
+      assert card.interval == Duration.parse!("PT2M")
+      assert card.ease_factor == 2.4
+      assert card.remaining_steps == 2
+      # assert card1.reps == 0
     end
   end
 end
