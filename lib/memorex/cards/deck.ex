e1cff0c85e98b0bf49cea74852d5651e41f7c966
Greg Woodward <woodward@users.noreply.github.com>
2022-06-01 06:49:41 -0700

    Separated out content from Deck into DeckParser

--- lib/memorex/cards/deck.ex
+++ lib/memorex/cards/deck.ex
@@ -3,8 +3,7 @@ defmodule Memorex.Cards.Deck do
 
   use Memorex.Schema
 
-  alias Memorex.Repo
-  alias Memorex.Cards.{Deck, Note}
+  alias Memorex.Cards.Note
 
   @type t :: %__MODULE__{
           id: Ecto.UUID.t() | nil,
@@ -21,50 +20,5 @@ defmodule Memorex.Cards.Deck do
     has_many :cards, through: [:notes, :cards]
 
     timestamps()
-  end
-
-  @spec read_file(String.t(), Deck.t() | nil) :: :ok
-  def read_file(filename, deck \\ nil) do
-    filename
-    |> File.read!()
-    |> Note.parse_file_contents(deck)
-  end
-
-  @spec read_dir(String.t()) :: :ok
-  def read_dir(dirname) do
-    deck_name = Path.basename(dirname)
-    deck = Repo.insert!(%__MODULE__{name: deck_name})
-
-    Path.wildcard(dirname <> "/*.md")
-    |> Enum.each(&read_file(&1, deck))
-  end
-
-  @spec read_note_dirs([String.t()] | nil) :: :ok
-  def read_note_dirs(note_dirs \\ nil) do
-    Note.clear_parse_flags()
-
-    note_dirs = if note_dirs, do: note_dirs, else: Application.get_env(:memorex, Memorex.Note)[:note_dirs]
-
-    note_dirs
-    |> Enum.each(fn dir ->
-      {:ok, files_and_dirs} = File.ls(dir)
-
-      files_and_dirs
-      |> Enum.map(fn file_or_dir ->
-        pathname = Path.join(dir, file_or_dir)
-        {:ok, file_stat} = File.stat(pathname)
-
-        case file_stat.type do
-          :regular ->
-            deck = Repo.insert!(%__MODULE__{name: Path.rootname(file_or_dir)})
-            read_file(pathname, deck)
-
-          :directory ->
-            read_dir(pathname)
-        end
-      end)
-    end)
-
-    Note.delete_notes_without_flag_set()
   end
 end
---
+++ test/memorex/deck_parser_test.exs
@@ -0,0 +1,80 @@
+defmodule Memorex.DeckParserTest do
+  @moduledoc false
+  use Memorex.DataCase
+
+  alias Memorex.Cards.{Card, Deck, Note}
+  alias Memorex.DeckParser
+
+  describe "read_file" do
+    test "a file gets converted into notes" do
+      DeckParser.read_file("test/fixtures/deck1.md")
+
+      assert Repo.all(Note) |> length() == 3
+      assert Repo.all(Card) |> length() == 6
+    end
+
+    test "can take an optional deck" do
+      deck = Repo.insert!(%Deck{name: "My Deck"})
+      DeckParser.read_file("test/fixtures/deck1.md", deck)
+
+      deck = Repo.all(Deck) |> Repo.preload(:notes) |> Repo.preload(:cards) |> List.first()
+
+      assert deck.name == "My Deck"
+      assert deck.notes |> length() == 3
+      assert deck.cards |> length() == 6
+    end
+  end
+
+  describe "read_dir" do
+    test "all of the files in a directory get incorporated into the deck" do
+      DeckParser.read_dir("test/fixtures/deck")
+
+      deck = Repo.all(Deck) |> List.first()
+      assert deck.name == "deck"
+
+      [note1, note2] = Repo.all(Note, order_by: :id) |> Repo.preload(:deck)
+      assert note1.deck.name == "deck"
+      assert note2.deck.name == "deck"
+
+      assert Repo.all(Card) |> length() == 4
+    end
+  end
+
+  describe "read_multiple_dirs" do
+    test "reads in notes from the multiple dirs in the config" do
+      DeckParser.read_note_dirs()
+
+      decks = Repo.all(Deck)
+      deck_names = decks |> Enum.map(& &1.name) |> Enum.sort()
+      assert deck_names == ["deck-1", "deck-2", "deck-3", "deck-4", "deck-5", "deck-6"]
+
+      assert Repo.all(Note) |> length() == 13
+      assert Repo.all(Card) |> length() == 26
+    end
+
+    test "deletes notes that are no longer present in the files" do
+      notes_dir = "test/tmp"
+      File.mkdir(notes_dir)
+      filename = "notes.md"
+      notes_file = Path.join(notes_dir, filename)
+
+      file_contents = """
+      one ⮂ 1
+      """
+
+      File.write!(notes_file, file_contents)
+
+      DeckParser.read_note_dirs([notes_dir])
+      assert Repo.all(Note) |> length() == 1
+
+      edited_file_contents = """
+      one ⮂ edited
+      """
+
+      File.write!(notes_file, edited_file_contents)
+
+      DeckParser.read_note_dirs([notes_dir])
+      assert Repo.all(Note) |> length() == 1
+    end
+  end
+end
