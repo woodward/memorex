a0748d5e1264ecb49becdc097ac0fb525ad336e4
Greg Woodward <woodward@users.noreply.github.com>
2022-06-07 19:55:58 -0700

    Verify that the sample config file works (it didn't)

--- test/memorex/config_file_test.exs
+++ test/memorex/config_file_test.exs
@@ -12,5 +12,15 @@ defmodule Memorex.ConfigFileTest do
       assert config["max_reviews_per_day"] == 350
       assert config["learn_ahead_time_interval"] == "PT20M"
     end
+
+    test "the sample config file works" do
+      config = ConfigFile.read("deck_config.example.toml")
+
+      assert config["new_cards_per_day"] == 20
+      assert config["max_reviews_per_day"] == 200
+      assert config["learn_ahead_time_interval"] == "PT20M"
+      assert config["learn_steps"] == ["PT1M", "PT10M"]
+      assert config["relearn_steps"] == ["PT10M"]
+    end
   end
 end
