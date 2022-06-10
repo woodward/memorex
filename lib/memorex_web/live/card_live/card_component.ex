09bc971e5e36a9008810386ff0f12d458e16799f
Greg Woodward <woodward@users.noreply.github.com>
2022-06-10 13:56:07 -0700

    Show a dash if the step is nil

--- lib/memorex_web/live/card_live/card_component.ex
+++ lib/memorex_web/live/card_live/card_component.ex
@@ -4,4 +4,8 @@ defmodule MemorexWeb.CardLive.CardComponent do
   use MemorexWeb, :live_component
 
   alias Memorex.Cards.Card
+
+  @spec step(nil | non_neg_integer()) :: String.t() | non_neg_integer()
+  def step(nil), do: "-"
+  def step(step_number), do: step_number
 end
