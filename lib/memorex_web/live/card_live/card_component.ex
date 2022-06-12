c5e64d078ef8e67bb7d69297d50f0426d1d03b13
Greg Woodward <woodward@users.noreply.github.com>
2022-06-12 15:44:07 -0700

    Some cleanup on the display of decks and links

--- lib/memorex_web/live/card_live/card_component.ex
+++ lib/memorex_web/live/card_live/card_component.ex
@@ -4,8 +4,13 @@ defmodule MemorexWeb.CardLive.CardComponent do
   use MemorexWeb, :live_component
 
   alias Memorex.Domain.Card
+  alias Phoenix.LiveView.Socket
 
   @spec step(nil | non_neg_integer()) :: String.t() | non_neg_integer()
   def step(nil), do: "-"
   def step(step_number), do: step_number
+
+  @spec show_card_history_link?(Socket.t()) :: boolean()
+  def show_card_history_link?(%Socket{view: MemorexWeb.CardLive.Show} = _socket), do: false
+  def show_card_history_link?(_socket), do: true
 end
