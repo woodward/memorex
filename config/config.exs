716bb79633555ac803b8d009cb7addafd11a9a9b
Greg Woodward <woodward@users.noreply.github.com>
2023-10-10 18:17:25 -0700

    Upgrade config.exs to use esbuild 0.18.6

--- config/config.exs
+++ config/config.exs
@@ -31,7 +31,7 @@ config :dart_sass,
   ]
 
 config :esbuild,
-  version: "0.17.11",
+  version: "0.18.6",
   default: [
     args: ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
