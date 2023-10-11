c0db4a310c702b6f711123e6293e5b1551ccf986
Greg Woodward <woodward@users.noreply.github.com>
2023-10-10 18:04:34 -0700

    Show heex debug annotations in dev mode

--- config/dev.exs
+++ config/dev.exs
@@ -78,3 +78,6 @@ config :phoenix, :stacktrace_depth, 20
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
+
+# Include HEEx debug annotations as HTML comments in rendered markup
+config :phoenix_live_view, :debug_heex_annotations, true
